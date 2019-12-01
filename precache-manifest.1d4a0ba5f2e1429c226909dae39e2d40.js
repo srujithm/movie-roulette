1c4aeaf4d44427b59eba315e71353708bd08833b
smallidi <smallidi@cisco.com>
2019-12-01 22:17:12 +0530

    Updates

---
+++ precache-manifest.1d4a0ba5f2e1429c226909dae39e2d40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a725f74d48e74b4f9176d2758fd1fa0c",
+    "url": "/movie-roulette/index.html"
+  },
+  {
+    "revision": "a73d3cb03e7536316bf3",
+    "url": "/movie-roulette/static/css/main.fcc32ab0.chunk.css"
+  },
+  {
+    "revision": "c3d468f898756111d277",
+    "url": "/movie-roulette/static/js/2.d4f0f7ff.chunk.js"
+  },
+  {
+    "revision": "a73d3cb03e7536316bf3",
+    "url": "/movie-roulette/static/js/main.aa6b824c.chunk.js"
+  },
+  {
+    "revision": "96c10d2efd7888b86b34",
+    "url": "/movie-roulette/static/js/runtime-main.025a0d4e.js"
+  }
+]);
